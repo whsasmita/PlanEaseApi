eb1b55dd58610351c49086cde87fd9201396b90d
Wahyu Harta Sasmita <118668406+whsasmita@users.noreply.github.com>
2025-06-26 22:20:48 +0800

    feat: add profiles

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -4,6 +4,8 @@ namespace Database\Factories;
 
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\profile>
@@ -17,9 +19,23 @@ class ProfileFactory extends Factory
      */
     public function definition(): array
     {
+        $sourceImages = [
+            storage_path('app/profile_photos/dummy_photos1.png'),
+            storage_path('app/profile_photos/dummy_photos2.png'),
+        ];
+
+        $randomImagePath = fake()->randomElement($sourceImages);
+
+        $destinationFilename = 'profile_photos/' . Str::random(40) . '.png';
+
+        Storage::disk('public')->put(
+            $destinationFilename,
+            file_get_contents($randomImagePath)
+        );
+
         return [
             'user_id' => User::factory(),
-            'photo_profile' => fake()->imageUrl(200, 200, 'people'),
+            'photo_profile' => $destinationFilename,
             'position' => fake()->jobTitle(),
             'division' => fake()->company(),
         ];
