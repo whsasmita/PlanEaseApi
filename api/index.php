39cd94452aff2a346f8ccf72b6084d9188c4ee38
Wahyu Harta Sasmita <118668406+whsasmita@users.noreply.github.com>
2025-07-01 10:02:43 +0800

    aa

--- api/index.php
+++ api/index.php
@@ -13,6 +13,7 @@ $app = \Illuminate\Foundation\Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
+        apiPrefix: 'api', // Pastikan ini ada
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
